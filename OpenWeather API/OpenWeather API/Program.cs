a0d6de0e9d02ecd4bcd6122858023c1d41d5eae6
kendall chapman <jkc4310@gmail.com>
2023-10-05 22:26:52 -0400

    WeatherMap

--- OpenWeather API/OpenWeather API/Program.cs
+++ OpenWeather API/OpenWeather API/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
+namespace WeatherApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsettings.json");
+
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please enter your zipcode");
+            var zipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+
+            Console.WriteLine();
+
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
+        }
+    }
+}
